cba639097096452b74bde6b4c3aad44f74a1c289
Anora_TR_05\Paul John Thottungal <pauljohnthottungal@anorasolutions.com>
2024-08-22 10:20:31 +0530

    Editing in branch,added new file and removed comments from main

--- main.c
+++ main.c
@@ -2,11 +2,7 @@
 int add();
 int main()
 {   
-    //Line added by PaulJohn09
-    //User 2 is adding comment
     printf("%d", 10);
     add();
     return 0;
 }
-//This line is added in branch after the branch has been edited
-//Adding by the user Paul John 09
