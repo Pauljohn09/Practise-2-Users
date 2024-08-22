9cc83f2fc11266f90373dd1c49fbbdd13bc194fa
Anora_TR_05\Paul John Thottungal <pauljohnthottungal@anorasolutions.com>
2024-08-22 14:27:04 +0530

    ﻿updating functin

--- main.c
+++ main.c
@@ -4,6 +4,8 @@ int main()
 {   
     //I added my functionality by pauljohn09
     //Added function by Paul-Anora
+    //I added my functionality
+    //Edited function by Paul-John-Anora
     printf("%d", 10);
     add();
     return 0;
