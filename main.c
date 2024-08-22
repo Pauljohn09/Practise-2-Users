55e2cc16fea86a025289f3287041c72eac7f1c51
Anora_TR_05\Paul John Thottungal <pauljohnthottungal@anorasolutions.com>
2024-08-22 12:52:11 +0530

    Adding final change

--- main.c
+++ main.c
@@ -3,7 +3,11 @@ int add();
 int main()
 {   
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
     //Paul John have added Functionality
+=======
+    //Paul-Anora also added his functinaliy but it is not completed
+>>>>>>> Stashed changes
 =======
     //Paul-Anora also added his functinaliy but it is not completed
 >>>>>>> Stashed changes
