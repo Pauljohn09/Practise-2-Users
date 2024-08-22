604591fd9a2ae43b334bd69e25549fcf7f9c1cb1
Anora_TR_05\Paul John Thottungal <pauljohnthottungal@anorasolutions.com>
2024-08-22 12:54:33 +0530

    sdf

--- main.c
+++ main.c
@@ -2,18 +2,8 @@
 int add();
 int main()
 {   
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-    //Paul John have added Functionality
-=======
-    //Paul-Anora also added his functinaliy but it is not completed
->>>>>>> Stashed changes
-=======
-    //Paul-Anora also added his functinaliy but it is not completed
->>>>>>> Stashed changes
     printf("%d", 10);
     add();
     return 0;
 }
-//Adding one comment by Subish
-//adding comment by paul
+
